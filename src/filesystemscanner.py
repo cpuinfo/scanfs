dc28af17c9e5a8f2a3daf18074a3edcb99c1ff96
CPU Info <cpuinfo10@gmail.com>
2021-07-21 22:18:17 +0530

    Add callback on every node and docstrings

--- src/filesystemscanner.py
+++ src/filesystemscanner.py
@@ -1,12 +1,44 @@
+import os
+
+
 class FileSystemScannerException(Exception):
     pass
 
 
 class FileSystemScanner:
-    def __init__(self, fwpath: str) -> None:
+
+    """
+    FileSystemScanner
+
+    This scanner scans the folder in the filesystem
+    with various filetype filters and provides a hook
+    whenever the filetype is found in the provided path
+
+    Attributes:
+        fpath (str): Folder in the filesytem to scan
+
+    Raises:
+        FileSystemScannerException: [Exception when the
+        folder path fpath is empty or not valid]
+    """
+
+    def __init__(self, fpath: str) -> None:
         super().__init__()
-        self._check_str_param(fwpath)
-        self.fwpath = fwpath
+        self._check_str_param(fpath)
+        self.fpath = fpath
+
+    def scan_for(self, filetype, callback):
+        """
+        Scans for specific filetype in the nested folder
+
+        Args:
+            filetype ([type]): Filetype
+            callback (function): Function called when the filetype
+            is found
+        """
+        nodes = os.scandir(self.fpath)
+        for node in nodes:
+            callback(node)
 
     def _check_str_param(self, param):
         if not isinstance(param, str) or not param:
@@ -15,5 +47,11 @@ class FileSystemScanner:
             )
 
 
+def callback(node):
+    print(node.name)
+
+
 if __name__ == "__main__":
-    fss = FileSystemScanner("/")
+    fss = FileSystemScanner("/home/maker")
+    fss.scan_for("elf", callback)
+    print(help(fss.scan_for))
